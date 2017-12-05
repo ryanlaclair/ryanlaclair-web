5baf0c2b8c074d5a7350dcfa9fb6c751412fd994
Ryan LaClair <ryanlaclair@gmail.com>
2017-12-04 22:48:12 -0500

    refactored styling

--- js/nav.js
+++ js/nav.js
@@ -4,6 +4,6 @@ const navScroll = 350;
 $(window).on("scroll touchmove", () => {
     $("nav").toggleClass("nav-bar-collapsed", $(document).scrollTop() > 0);
 
-    $(".header-img").css("opacity", 1 - $(document).scrollTop() / headerScroll);
-    $(".nav-img").css("opacity", $(document).scrollTop() / navScroll);
+    $("#header-img").css("opacity", 1 - $(document).scrollTop() / headerScroll);
+    $("#nav-img-home").css("opacity", $(document).scrollTop() / navScroll);
 });
